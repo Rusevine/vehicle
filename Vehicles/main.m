a5caad4548381a5d8226b8fa90974ca4fe63ceae
Wiljay <wiljay.flores@gmail.com>
2018-08-04 22:54:22 -0400

    first commit

---
+++ Vehicles/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Vehicles
+//
+//  Created by Wiljay Flores on 2018-08-04.
+//  Copyright © 2018 wiljay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
